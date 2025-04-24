8dae8e751c00681cb8a736bfba71aa1ae5782475
SamuelJanas <sneakyinsect@gmail.com>
2025-04-24 15:39:30 +0200

    reading train/test

--- load_data.py
+++ load_data.py
@@ -3,16 +3,18 @@ import pandas as pd
 
 from tqdm import tqdm
 
-filenames_list = os.listdir('data/SMNI_CMI_TRAIN/') 
-EEG_data = pd.DataFrame({}) 
+def get_dataset(data_path: str) -> pd.DataFrame:
+    filenames_list = os.listdir(data_path) 
+    EEG_data = pd.DataFrame({}) 
 
-for file_name in tqdm(filenames_list):
-    if not file_name.endswith(".csv"):
-        continue
-    temp_df = pd.read_csv('data/SMNI_CMI_TRAIN/' + file_name)
-    EEG_data = pd.concat([EEG_data, temp_df])
+    for file_name in tqdm(filenames_list):
+        if not file_name.endswith(".csv"):
+            continue
+        temp_df = pd.read_csv(data_path + file_name)
+        EEG_data = pd.concat([EEG_data, temp_df])
 
-EEG_data = EEG_data.drop(columns=["Unnamed: 0"])
-# This will thro a warning but don't mind
-EEG_data.loc[EEG_data['matching condition'] == 'S2 nomatch,', 'matching condition'] =  'S2 nomatch'
+    EEG_data = EEG_data.drop(columns=["Unnamed: 0"])
+    # This will thro a warning but don't mind
+    EEG_data.loc[EEG_data['matching condition'] == 'S2 nomatch,', 'matching condition'] =  'S2 nomatch'
+    return EEG_data
 
---
+++ train.py
@@ -0,0 +1,5 @@
+from load_data import get_dataset
+
+train_data = get_dataset(data_path="data/SMNI_CMI_TRAIN/")
+test_data = get_dataset(data_path="data/SMNI_CMI_TEST/")
+
