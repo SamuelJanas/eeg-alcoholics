dbe7a640336245ebd11c297705d86ad15152b17a
Samuel Janas <sneakyinsect@gmail.com>
2025-05-15 12:42:30 +0200

    speed up data loading

--- load_data.py
+++ load_data.py
@@ -1,11 +1,11 @@
 import os
-import pickle
 import pandas as pd
 import hashlib
 
 from tqdm import tqdm
 
 def get_dataset(data_path: str) -> pd.DataFrame:
+    os.makedirs("tmp/", exist_ok=True)
     encoded_path = hashlib.sha256(data_path.encode("utf-8")).hexdigest()[:10]
     cache_path = f"tmp/{encoded_path}.pickle"
 
@@ -14,15 +14,17 @@ def get_dataset(data_path: str) -> pd.DataFrame:
         return pd.read_pickle(cache_path)
 
     filenames_list = os.listdir(data_path) 
+    temp_data = []
     EEG_data = pd.DataFrame({}) 
 
     for file_name in tqdm(filenames_list):
         if not file_name.endswith(".csv"):
             continue
-        temp_df = pd.read_csv(data_path + file_name)
-        EEG_data = pd.concat([EEG_data, temp_df])
+        temp_df = pd.read_csv(data_path + file_name, index_col=0)
+        temp_data.append(temp_df)
 
-    EEG_data = EEG_data.drop(columns=["Unnamed: 0"])
+    EEG_data = pd.concat(temp_data)
+
     # This will thro a warning but don't mind
     EEG_data.loc[EEG_data['matching condition'] == 'S2 nomatch,', 'matching condition'] =  'S2 nomatch'
 
